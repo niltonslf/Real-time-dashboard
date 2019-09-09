1eb7e3410fcf906fe9b85627bdbeb8f6af85df09
Nilton Lopes <niltonrck@gmail.com>
2019-09-09 10:44:33 -0300

    WIP: Corrige bot com os dados certos das requests

--- src/bot.js
+++ src/bot.js
@@ -1,21 +1,28 @@
 const fetch = require('node-fetch')
 
-function makeRequest(serial, speed) {
+/**
+ * Prepara corpo da requisição
+ * @param {*} serial
+ * @param {*} speed
+ */
+function makeRequest(marcha, rpm) {
   fetch('http://localhost:3333/bike', {
     headers: { 'Content-Type': 'application/json; charset=utf-8' },
     method: 'POST',
-    body: JSON.stringify({
-      serial: serial,
-      marcha: speed * 2,
-      somatorio_de_voltas: speed,
-      date_time: 'data'
-    })
+    body: JSON.stringify({ marcha, rpm })
   })
 }
 
+/**
+ * Realiza requisição
+ */
 setInterval(function() {
   for (let i = 1; i <= 30; i++) {
-    makeRequest(i, Math.round(Math.random() * 100))
+    makeRequest(
+      i,
+      Math.ceil(Math.random() * 24),
+      Math.ceil(Math.random() * 200)
+    )
     console.log(`🚴\t Request realizado`)
   }
 }, 1000)
